37be6d57064109e7f13ce73caf80f194c82d54ce
EasyG0312 <aziko77kg@gmail.com>
2026-04-22 19:22:40 +0600

    docs: add development todo list

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+// Force rebuild v2.1.3 - commit e0f147c
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
